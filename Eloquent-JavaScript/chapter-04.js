45ba8cf73f91dfea1ad00c23d01a733d8762931c
JimySheepman <y.alikoyuncu@gmail.com>
2021-09-09 15:41:33 +0300

    commit #3

--- Eloquent-JavaScript/chapter-04.js
+++ Eloquent-JavaScript/chapter-04.js
@@ -105,3 +105,108 @@ function tableFor(event, journal) {
 }
 
 console.log(tableFor("pizza", journal));
+
+for (let i = 0; i < journal.length; i++) {
+  let entry = journal[i];
+}
+
+for (let entry of journal) {
+  console.log(`${entry.events.length} events.`);
+}
+
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+console.log(journalEvents(journal));
+
+for (let event of journalEvents(journal)) {
+  console.log(event + ":", phi(tableFor(event, journal)));
+}
+
+let todolist = [];
+function remember(task) {
+  todolist.push(task);
+}
+function getTask() {
+  return todolist.shift();
+}
+function rememberUrgently(task) {
+  todolist.unshift(task);
+}
+
+remember("groceries");
+getTask();
+console.log(todolist);
+
+function remove(array, index) {
+  return array.slice(0, index).concat(array.slice(index + 1));
+}
+console.log(remove(["a", "b", "c", "d", "e"], 2));
+
+
+console.log("coconuts".slice(4, 7));
+console.log("coconut".indexOf("u"));
+console.log("one two three".indexOf("ee"));
+console.log("  okay \n ".trim());
+console.log(String(6).padStart(3, "0"));
+let sentence = "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words);
+console.log(words.join(". "));
+console.log("LA".repeat(3));
+let string = "abc";
+console.log(string.length);
+console.log(string[1]);
+
+
+function max(...numbers){
+  let result = -Infinity;
+  for (let number of numbers){
+    if(number > result){
+      result = number;
+    }
+  }
+  return result;
+}
+
+console.log(max(4,2,1,5,6,78,8,1));
+
+let numbers = [12,5,6,123,6,7,1234,1];
+console.log(max(...numbers));
+
+
+function randomPointOnCircle(radius) {
+  let angle = Math.random() * 2 *Math.PI;
+  return{
+    x: radius * Math.cos(angle),
+    y : radius * Math.sin(angle)};
+}
+
+console.log(randomPointOnCircle(4));
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10));
+
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2]));
+}
+
+
+let _string = JSON.stringify({squirrel: false,events: ["weekend"]});
+console.log(_string);
+console.log(JSON.parse(_string).events);
