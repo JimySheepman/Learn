f1ef57f400cf775df8b9c06e245157b3921a78f2
JimySheepman <y.alikoyuncu@gmail.com>
2021-12-20 18:34:24 +0300

    #2

--- patika.dev/nodejs/circleArea.js
+++ patika.dev/nodejs/circleArea.js
@@ -2,7 +2,7 @@ const arguments = process.argv.slice(2)
 
 function calculateArea(radius) {
     let pi = Math.PI
-    let area = 2*pi*radius
+    let area = pi*radius*radius
     console.log("Area: ",area.toFixed(3))
 }
 
