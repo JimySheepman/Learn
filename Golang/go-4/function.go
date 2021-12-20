9ed39ca456b7d0f2e573dfb1917a4da7b8f4d523
JimySheepman <y.alikoyuncu@gmail.com>
2021-12-21 00:17:38 +0300

    #1

---
+++ Golang/go-4/function.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusplus(a, b, c int) int {
+	return a + b + c
+}
+
+func main() {
+	res := plus(1, 2)
+	fmt.Println("1+2=", res)
+
+	res = plusplus(1, 2, 3)
+	fmt.Println("1+2+3=", res)
+}
