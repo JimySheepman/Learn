5e33fb13976442996bf6af0a9efeb74884bd2dbb
JimySheepman <y.alikoyuncu@gmail.com>
2021-12-25 12:04:30 +0300

    #1

---
+++ Golang/go-4/recover.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func mayPanic() {
+	panic("a problem")
+}
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recoverd. Error:\n", r)
+		}
+	}()
+	mayPanic()
+	fmt.Println("After mayPanic()")
+}
