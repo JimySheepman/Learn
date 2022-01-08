dcc0e0cfdc6505e5c401699f3c6b0370ce47d8a8
JimySheepman <y.alikoyuncu@gmail.com>
2022-01-08 16:07:48 +0300

    #1

---
+++ API/fixturesAPI/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
+
+const app = express();
+
+//Connect DB
+mongoose.connect("mongodb://localhost:27017/fixtures").then(() => {
+  console.log("Db connceted succed!");
+});
+
+//Global Variable
+global.userIN = null;
+
+//Middlewares
+app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost:27017/fixtures",
+    }),
+  })
+);
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
+//Routes
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is started port:${PORT}`);
+});
