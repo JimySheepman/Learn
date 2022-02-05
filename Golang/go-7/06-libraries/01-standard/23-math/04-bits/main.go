c56ccb76163e097d6e8638c4b4207260a9341788
JimySheepman <y.alikoyuncu@gmail.com>
2022-02-05 14:19:01 +0300

    #1

---
+++ Golang/go-7/06-libraries/01-standard/23-math/04-bits/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"math/bits"
+)
+
+func main() {
+	bits.Add()
+	bits.Div()
+	bits.LeadingZeros()
+	bits.Len()
+	bits.Mul()
+	bits.Rem()
+	bits.TrailingZeros()
+	bits.Sub()
+	bits.RotateLeft()
+	bits.Reverse()
+	bits.ReverseBytes()
+	bits.OnesCount()
+}
