85789018c802e7fe4bed2e2e6873609c208cd7c2
JimySheepman <y.alikoyuncu@gmail.com>
2022-02-05 14:07:48 +0300

    #1

---
+++ Golang/go-7/06-libraries/01-standard/21-sort/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+func main() {
+	Sample("Sort", Sort)
+	Sample("Search", Search)
+	Sample("Slice", Slice)
+	Sample("Strings", Strings)
+	Sample("StringsAreSorted", StringsAreSorted)
+}
+
+func Sort() {
+	people := []Person{
+		{"Bob", 31},
+		{"John", 42},
+		{"Michael", 17},
+		{"Jenny", 26},
+	}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Age > people[j].Age
+	})
+	fmt.Println(people)
+}
+
+func Search() {
+	a := []int{1, 6, 10, 15, 21, 28, 36, 45, 55}
+	x := 6
+
+	i := sort.Search(len(a), func(i int) bool { return a[i] >= x })
+	if i < len(a) && a[i] == x {
+		fmt.Printf("found %d at index %d in %v\n", x, i, a)
+	} else {
+		fmt.Printf("%d not found in %v\n", x, a)
+	}
+}
+
+func Slice() {
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Gopher", 7},
+		{"Alice", 55},
+		{"Vera", 24},
+		{"Bob", 75},
+	}
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Println("By name:", people)
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	fmt.Println("By age:", people)
+}
+
+func Strings() {
+	s := []string{"Go", "Bravo", "Gopher", "Alpha", "Grin", "Delta"}
+	sort.Strings(s)
+	fmt.Println(s)
+}
+
+func StringsAreSorted() {
+	s := []string{"Alice", "Bob"}
+	isSorted := sort.StringsAreSorted(s)
+	fmt.Println(isSorted)
+}
+
+func Sample(name string, fn func()) {
+	fmt.Println(">", name)
+	fn()
+	fmt.Println()
+}
