4eba1975e912abcf66150d372c5622b4a8808089
JimySheepman <y.alikoyuncu@gmail.com>
2022-03-14 23:23:26 +0300

    #1

---
+++ C/CLang/cp-1/reverse.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+void reverse(char s[]);
+
+int main(void)
+{
+    char line[MAXLINE]; /* current input line */
+
+    while (get_line(line, MAXLINE) > 0)
+    {
+        reverse(line);
+        printf("%s\n", line);
+    }
+
+    return 0;
+}
+
+int get_line(char s[], int lim)
+{
+    int c, i, l;
+
+    for (i = 0, l = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+        if (i < lim - 1)
+            s[l++] = c;
+    if (c == '\n' && l < lim - 1)
+        s[l++] = c;
+    s[l] = '\0';
+
+    return l;
+}
+
+void reverse(char s[])
+{
+    int i, j;
+    int tmp;
+
+    for (j = 0; s[j] != '\0'; ++j)
+        ;
+    --j;
+    if (s[j - 1] == '\n')
+        --j;
+
+    for (i = 0; i < j; ++i, --j)
+    {
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+}
--- C/CLang/cp-1/temprerature.c
+++ C/CLang/cp-1/temprerature.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+float ftoc(float fahr);
+
 int main(void)
 {
     float fahr, celsius;
     float lower, upper, step;
-
     lower = 0;
     upper = 300;
     step = 20;
-
-    printf("  F      C\n");
-
     fahr = lower;
+
     while (fahr <= upper)
     {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        celsius = ftoc(fahr);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
     return 0;
+}
+
+float ftoc(float fahr)
+{
+    float celsius;
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    return celsius;
 }
