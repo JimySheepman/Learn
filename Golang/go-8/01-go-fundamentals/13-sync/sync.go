aea2827a810bcd8436ff8b18cb2494dad6696145
JimySheepman <y.alikoyuncu@gmail.com>
2022-03-14 13:04:26 +0300

    #1

---
+++ Golang/go-8/01-go-fundamentals/13-sync/sync.go
@@ -0,0 +1,22 @@
+package main
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
