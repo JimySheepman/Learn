12e17f5faaf46897e757c0d6d5b22402299398d4
JimySheepman <y.alikoyuncu@gmail.com>
2022-03-30 23:56:04 +0300

    #1

---
+++ C/CLang/cp-5/07/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lines.h"
+#include "qsort.h"
+
+char *lineptr[MAXLINES]; /* pointers to text lines */
+
+/* sort input lines */
+int main(void)
+{
+    int nlines; /* number of input lines read */
+    int i;
+
+    char s[MAXLINES][MAXLEN];
+
+    for (i = 0; i < MAXLINES; ++i) {
+        *(lineptr + i) = s[i];
+    }
+
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort(lineptr, 0, nlines-1);
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
---
+++ C/CLang/cp-5/07/qsort.h
@@ -0,0 +1,1 @@
+void qsort(char *lineptr[], int left, int right);
