d538a1472b9693a174276d0bcd35f8d39f6b0558
JimySheepman <y.alikoyuncu@gmail.com>
2022-03-30 23:55:31 +0300

    #1

---
+++ C/CLang/cp-5/05/strncmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int _strncmp(char *s, char *t, int n);
+
+int main(void)
+{
+    char *s, *t;
+
+    s = "testabc";
+    t = "testddc";
+
+    printf("%s cmp %s: %d\n", s, t, _strncmp(s, t, 5));
+
+    return 0;
+}
+
+int _strncmp(char *s, char *t, int n)
+{
+    for (; n-- > 0 && *s == *t; ++s, ++t)
+        if (*s == '\0' || n == 0)
+            return 0;
+    return *s - *t;
+}
