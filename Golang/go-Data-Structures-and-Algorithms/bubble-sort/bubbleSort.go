5ee7748273de518ff26a88c4e14bf7e442918165
JimySheepman <y.alikoyuncu@gmail.com>
2022-04-12 01:03:08 +0300

    data strucruters and algorithms  examples

---
+++ Golang/go-Data-Structures-and-Algorithms/bubble-sort/bubbleSort.go
@@ -0,0 +1,14 @@
+package bubble_sort
+
+func BubbleSort(array []int) {
+	swapCount := 1
+	for swapCount > 0 {
+		swapCount = 0
+		for itemIndex := 0; itemIndex < len(array)-1; itemIndex++ {
+			if array[itemIndex] > array[itemIndex+1] {
+				array[itemIndex], array[itemIndex+1] = array[itemIndex+1], array[itemIndex]
+				swapCount += 1
+			}
+		}
+	}
+}
---
+++ Golang/go-Data-Structures-and-Algorithms/insertion-sort/insertionSort.go
@@ -0,0 +1,11 @@
+package insertion_sort
+
+func InsertionSort(array []int) {
+	for itemIndex, itemValue := range array {
+		for itemIndex != 0 && array[itemIndex-1] > itemValue {
+			array[itemIndex] = array[itemIndex-1]
+			itemIndex -= 1
+		}
+		array[itemIndex] = itemValue
+	}
+}
