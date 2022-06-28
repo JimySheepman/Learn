5d243264f8e20752115892be96fb685fd8e149c6
JimySheepman <y.alikoyuncu@gmail.com>
2022-06-28 12:48:16 +0300

    learning Java

---
+++ Java/zero2hero/src/second_ten_classes/Files.java
@@ -0,0 +1,44 @@
+package second_ten_classes;
+
+import java.io.*;
+
+public class Files {
+
+
+    public void copyFile() throws IOException{
+        try {
+            FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        }finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    public void coptFile2() throws IOException{
+        FileReader in = null;
+        FileWriter out = null;
+
+        try {
+            in = new FileReader("input.txt");
+            out = new FileWriter("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        }finally {
+                in.close();
+                out.close();
+        }
+    }
+}
---
+++ Java/zero2hero/src/second_ten_classes/Main.java
@@ -0,0 +1,12 @@
+package second_ten_classes;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Files files = new Files();
+        files.copyFile();
+
+    }
+}
