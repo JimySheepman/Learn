80e34985c06397206acfb5ce8f80f0fba221f11a
JimySheepman <y.alikoyuncu@gmail.com>
2022-07-29 11:29:43 +0300

    learning Java

---
+++ Java/java8/src/base/Base64Class.java
@@ -0,0 +1,40 @@
+package base;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.UUID;
+
+public class Base64Class {
+
+public void printAll(){
+
+    try {
+
+        // Encode using basic encoder
+        String base64encodedString = Base64.getEncoder().encodeToString(
+                "TutorialsPoint?java8".getBytes("utf-8"));
+        System.out.println("Base64 Encoded String (Basic) :" + base64encodedString);
+
+        // Decode
+        byte[] base64decodedBytes = Base64.getDecoder().decode(base64encodedString);
+
+        System.out.println("Original String: " + new String(base64decodedBytes, "utf-8"));
+        base64encodedString = Base64.getUrlEncoder().encodeToString(
+                "TutorialsPoint?java8".getBytes("utf-8"));
+        System.out.println("Base64 Encoded String (URL) :" + base64encodedString);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 10; ++i) {
+            stringBuilder.append(UUID.randomUUID().toString());
+        }
+
+        byte[] mimeBytes = stringBuilder.toString().getBytes("utf-8");
+        String mimeEncodedString = Base64.getMimeEncoder().encodeToString(mimeBytes);
+        System.out.println("Base64 Encoded String (MIME) :" + mimeEncodedString);
+
+    } catch(UnsupportedEncodingException e) {
+        System.out.println("Error :" + e.getMessage());
+    }
+}
+}
--- Java/java8/src/base/Main.java
+++ Java/java8/src/base/Main.java
@@ -3,7 +3,7 @@ package base;
 public class Main {
 
     public static void main(String[] args) {
-        LambdaExpressions lambdaExpressions =new LambdaExpressions();
+        LambdaExpressions lambdaExpressions = new LambdaExpressions();
         lambdaExpressions.printAll();
 
         MethodReference methodReference = new MethodReference();
@@ -23,5 +23,8 @@ public class Main {
 
         NashornJavaScript nashornJavaScript = new NashornJavaScript();
         nashornJavaScript.printAll();
+
+        Base64Class base64Class = new Base64Class();
+        base64Class.printAll();
     }
 }
