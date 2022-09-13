a3a62d66d7e48c7b61bc384ff20cd526fa7136f8
JimySheepman <y.alikoyuncu@gmail.com>
2022-09-13 23:07:05 +0300

    leetcode

---
+++ Coding-Challenge/leetcode/83.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	var cur *ListNode = head
+
+	for cur.Next != nil {
+		if cur.Next.Val == cur.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return head
+}
