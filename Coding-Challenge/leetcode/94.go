855909b8c16247355b3f225775f4b01a419a976b
JimySheepman <y.alikoyuncu@gmail.com>
2023-01-14 19:04:52 +0300

    leetcode

---
+++ Coding-Challenge/leetcode/94.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	return append(append(inorderTraversal(root.Left), root.Val), inorderTraversal(root.Right)...)
+}
