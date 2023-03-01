0b3384d2d2449124b4e58addcf59d3250d1249f9
JimySheepman <y.alikoyuncu@gmail.com>
2023-03-01 17:32:02 +0300

    add concurrency example

---
+++ Golang/go-concurrency/01-concurrency-patterns/5-select/example-2/select.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	c := boring("Joe")
+
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("You're too slow.")
+			return
+		}
+	}
+}
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
